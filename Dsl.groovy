3b975ef862f71f3cb0026c82b82ee9a3b03924ef
Insha k n <55205426+inshakn@users.noreply.github.com>
2022-11-25 11:50:09 +0530

    Update Dsl.groovy

--- Dsl.groovy
+++ Dsl.groovy
@@ -1,7 +1,15 @@
+SCM_REPO = 'https://github.com/inshakn/first_dsl.git'
 job("example_DSLjob"){
   description("first DSL job at ${new Date()}")
-  triggers{
-    scm('* * * * *')
+  scm{
+    git{
+          remote {
+                    name('origin')
+                    url('${SCM_REPO}')
+                        }
+                        branch("main")
+                    }
+    }
   }
   steps{
       shell('echo hello insha')
